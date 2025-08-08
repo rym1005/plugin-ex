6e2daf001a0f8c582e8ab57192050e049a04dd5b
rymin <happyrym@gmail.com>
2025-08-08 16:48:02 +0900

    test app 추가

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by rymin on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
